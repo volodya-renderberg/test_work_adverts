7d86b82b3fef4bc9271bccd08b081a6aa1c8152a
volodya-renderberg <volodya.renderberg@gmail.com>
2021-04-26 16:26:09 +0300

    primary keys - city, category

--- main_app/models.py
+++ main_app/models.py
@@ -5,13 +5,13 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50, primary_key=True)
 
     def __str__(self):
             return f'{self.name}'
 
 class City(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50, primary_key=True)
 
     def __str__(self):
             return f'{self.name}'
